f0e2a1cdb147a04be1212810cc2a95b78f6a2bcd
samundar <samundarsingh9525@gmail.com>
2022-01-18 18:22:10 +0530

    save1

--- routing/routingDemo/src/app/app-routing.module.ts
+++ routing/routingDemo/src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path:"dashboard",
+    component:DashboardComponent
+  },
+  {
+    path:"login",
+    component:LoginComponent
+  },
+
+  {
+    path:"aboutus",
+    component:AboutusComponent
+  },
+  {
+    path:"/",
+    component:AppComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
