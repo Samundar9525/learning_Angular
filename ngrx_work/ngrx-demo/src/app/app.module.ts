568106c33560daf195f2353a8dfec860d392f1cc
samundar <samundarsingh9525@gmail.com>
2022-01-19 23:07:36 +0530

    bootstap added

--- ngrx_work/ngrx-demo/src/app/app.module.ts
+++ ngrx_work/ngrx-demo/src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { LoginComponent } from './login/login.component';
+import { UserloginComponent } from './userlogin/userlogin.component';
+import { HeaderComponent } from './header/header.component';
+import {HttpClientModule} from "@angular/common/http"
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    UserloginComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
